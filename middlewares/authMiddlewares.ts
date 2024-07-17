3aa4487e9ce5eae3d6c0b010cca4f178d6a94f85
thibautlecee <leceethibaut@gmail.com>
2024-07-17 19:06:35 +0200

    docker compose corsOptions

--- middlewares/authMiddlewares.ts
+++ middlewares/authMiddlewares.ts
@@ -1,39 +1,39 @@
-import { Request, Response, NextFunction } from 'express';
-import { decode } from 'jsonwebtoken';
-import { PrismaClient } from '@prisma/client';
-import { disconnectPrisma } from '../utils/disconnectPrismaClient';
+import {Request, Response, NextFunction} from 'express';
+import {decode} from 'jsonwebtoken';
+import {PrismaClient} from '@prisma/client';
+import {disconnectPrisma} from '../utils/disconnectPrismaClient';
 
 const prisma = new PrismaClient()
 
 export function authHandler(roles: string[]): (req: Request, res: Response, next: NextFunction) => void {
-  return async (req: Request, res: Response, next: NextFunction) => {
-    //Récup la requete avec le bearer
-    console.log("T'es qui grooos ? : ")
+    return async (req: Request, res: Response, next: NextFunction) => {
+        //Récup la requete avec le bearer
+        console.log("T'es qui grooos ? : ")
 
-    let authBearer = req.headers.authorization?.split("Bearer ")[1];
+        let authBearer = req.headers.authorization?.split("Bearer ")[1];
 
-    console.log("C'est toi le auth bearer ? : ", authBearer)
+        console.log("C'est toi le auth bearer ? : ", authBearer)
 
-    if (!authBearer) return res.status(401).end();
+        if (!authBearer) return res.status(401).end();
 
-    let token: any = decode(authBearer)
+        let token: any = decode(authBearer)
 
-    if (!token) return res.status(401).end();
-    console.log("Decodeur: ", token)
-
-
-    let existngUser = await prisma.user.findUnique({ where: { id: token.id } })
+        if (!token) return res.status(401).end();
+        console.log("Decodeur: ", token)
 
 
-    disconnectPrisma(prisma)
+        const existngUser = await prisma.user.findUnique({where: {id: token.id}})
 
 
-    if (roles.includes(token.role) && existngUser) {
-      next();
-    } else {
-      return res.status(401).end();
+        await disconnectPrisma(prisma)
+
+
+        if (roles.includes(token.role) && existngUser) {
+            next();
+        } else {
+            return res.status(401).end();
+        }
     }
-  }
 
 }
 
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,18 @@
 import express from "express";
+import cors from "cors";
 // import userRouter from "./routes/user.routes";
-import authRouter from "./routes/auth.routes.";
-import { errorHandler } from "../middlewares/errorMiddlewares";
+import authRouter from "./routes/auth.routes";
+import {errorHandler} from "../middlewares/errorMiddlewares";
+
 const app = express()
 const port = process.env.PORT || 80;
+const allowedOrigins = ['http://localhost:3000'];
 
+const options: cors.CorsOptions = {
+    origin: allowedOrigins
+};
+
+app.use(cors(options))
 app.use(express.json())
 app.use("/auth", authRouter)
 
@@ -13,7 +21,6 @@ app.use("/uploads", express.static("./uploads"));
 app.use(errorHandler)
 
 
-
-app.listen(port, () =>{
-  console.log(`Server running on http://localhost:${port}`)
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`)
 })  
