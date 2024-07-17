465b2e797b35fe9406737068726fcc3f733dfac9
BoutroisJordan <jordanboutroispro@gmail.com>
2024-07-17 21:17:35 +0200

    fix conflcits

--- middlewares/authMiddlewares.ts
+++ middlewares/authMiddlewares.ts
@@ -6,18 +6,11 @@ import {disconnectPrisma} from '../utils/disconnectPrismaClient';
 const prisma = new PrismaClient()
 
 export function authHandler(roles: string[]): (req: Request, res: Response, next: NextFunction) => void {
-<<<<<<< HEAD
   return async (req: Request, res: Response, next: NextFunction) => {
 
     //Récup la requete avec le bearer
     let authBearer = req.headers.authorization?.split("Bearer ")[1];
-=======
-    return async (req: Request, res: Response, next: NextFunction) => {
-        //Récup la requete avec le bearer
-        console.log("T'es qui grooos ? : ")
 
-        let authBearer = req.headers.authorization?.split("Bearer ")[1];
->>>>>>> feature/demande
 
         console.log("C'est toi le auth bearer ? : ", authBearer)
 
@@ -31,12 +24,6 @@ export function authHandler(roles: string[]): (req: Request, res: Response, next
 
         const existngUser = await prisma.user.findUnique({where: {id: token.id}})
 
-
-<<<<<<< HEAD
-=======
-        await disconnectPrisma(prisma)
-
->>>>>>> feature/demande
 
         if (roles.includes(token.role) && existngUser) {
             next();
