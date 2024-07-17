9a67d535264627f9000188e29dc7ed9cb42bdf9d
thibautlecee <leceethibaut@gmail.com>
2024-07-17 19:32:45 +0200

    fix conflicts again

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,10 +1,9 @@
-import { Router } from 'express';
-<<<<<<< HEAD
-import { Login, Register, test } from '../controllers/auth.controller';
-import { userLoginSchema, userRegistrationSchema } from '../../validators/auth.validator';
-import { authHandler } from '../../middlewares/authMiddlewares';
+import {Router} from 'express';
+import {Login, Register, test} from '../controllers/auth.controller';
+import {userLoginSchema, userRegistrationSchema} from '../../validators/auth.validator';
+import {authHandler} from '../../middlewares/authMiddlewares';
 import upload from '../../config/multer';
-import { validateData } from '../../middlewares/validatorMiddlewares';
+import {validateData} from '../../middlewares/validatorMiddlewares';
 
 const authRouter = Router();
 
@@ -14,18 +13,4 @@ authRouter.post('/register', upload.single("photo"), validateData(userRegistrati
 
 // authRouter.get('/test', authHandler(["otrerole", "admin"]), test);
 
-export default authRouter;
 
-=======
-import { login, register, test } from '../controllers/auth.controller';
-import { authSignUpValidator, authValidator } from '../../validators/auth.validator';
-import { authHandler } from '../../middlewares/authMiddlewares';
-
-const authRouter = Router();
-
-authRouter.post('/login', authValidator, login);
-authRouter.post('/register', authSignUpValidator, register);
-authRouter.get('/test', authHandler(["otrerole", "admin"]), test);
-
-export default authRouter;
->>>>>>> feature/demande
