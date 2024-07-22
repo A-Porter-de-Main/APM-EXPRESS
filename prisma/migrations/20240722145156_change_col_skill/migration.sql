17e7d2d75c7a68ca56101b8ce682a7b3dfdc1751
BoutroisJordan <jordanboutroispro@gmail.com>
2024-07-22 16:53:52 +0200

    seed skill ok

---
+++ prisma/migrations/20240722145156_change_col_skill/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Skill" ALTER COLUMN "parentId" SET DATA TYPE TEXT;
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,12 +1,13 @@
 import { PrismaClient } from "@prisma/client";
 import { RoleCreateDTO } from "../src/types/role";
 import { disconnectPrisma } from "../utils/disconnectPrismaClient";
-
+import { skills } from "../data/skills"
 const prisma = new PrismaClient();
 
 
 async function main(callback: () => void) {
   await SeedRoles()
+  await SeedSkills()
 
   callback()
 }
@@ -21,10 +22,43 @@ const SeedRoles = async () => {
   ]
 
   roles.map(async (role: RoleCreateDTO) => {
+    let findExisting = await prisma.role.findUnique({ where: { name: role.name } })
+    if (findExisting) return;
     const createdRole = await prisma.role.create({ data: role })
   })
   console.log("Role Seeding Success")
 
 }
+
+const SeedSkills = async () => {
+
+  const copySkills = [...skills];
+
+  for (const item of copySkills) {
+    let findExisting = await prisma.skill.findUnique({ where: { id: item.id } })
+    if (findExisting) return;
+    const oldId = item.id;
+
+
+    const created = await prisma.skill.create({
+      data: {
+        name: item.name,
+        description: item.description,
+        parentId: item.parentId ? item.parentId : null
+      }
+    })
+
+
+    copySkills.map((oneSkill) => {
+      if (oneSkill.parentId === oldId) oneSkill.parentId = created.id;
+    })
+
+  }
+
+  console.log("Role Seeding Success")
+
+}
+
+
 
 main(() => disconnectPrisma(prisma))
